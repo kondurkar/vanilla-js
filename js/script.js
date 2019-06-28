e6263d53613d71493fd7a8ae4c0426182996ca16
Yogesh <yogesh@webmaffia.com>
2019-06-28 11:35:15 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,10 +1,46 @@
 'use strict';
 
-let navItems = document.querySelectorAll('.nav-item');
-document.querySelectorAll('.nav-item').onclick = function () { 
-    tabFunction();
-};
+const navItems = document.querySelectorAll('.nav-item');
 
-function tabFunction() {
-    console.log(this);
+
+
+//tab navigation
+navItems.forEach((navItem, index) => {
+    navItem.addEventListener('click', function () {
+        const children = navItem.children;
+        //children.innerHTML.classList.add('active');
+        //console.log(children.nodeValue);
+        
+        document.querySelectorAll('.nav-link').classList.add('active');
+        this.querySelector('.nav-link').classList.add('active');
+    });
+});
+
+
+
+let formBtn1 = document.getElementById('form-btn1');
+
+formBtn1.addEventListener('click', function(){
+    validateForm();
+});
+
+//form validation
+function validateForm() {
+    let fullName = document.getElementById('fullName').value;
+    let email = document.getElementById('email').value;
+    let dob = document.getElementById('dob').value;
+    let gender = document.querySelector('input[name="gender"]:checked').value;
+    let adress = document.getElementById('adress').value;
+    let regEmail = /^([a-z0-9\-_\.])+@(([a-z0-9\-_])+\.)+([a-z0-9]{2,4})$/i;
+   
+    if (fullName == "") {
+        alert("Name must be filled out");
+    }
+    
+    if ( !regEmail.test(email) ) {
+        alert("email must be filled out");
+    }
 }
+
+//fetch values
+
